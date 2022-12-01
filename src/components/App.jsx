9cd80965e789c9d165c542c22d69f4f9c926de79
Chris Dopuch <chris.dopuch@zapier.com>
2022-11-30 18:56:20 -0800

    adds search functionality and no results found

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,17 @@ import './App.css';
 import { searchArtworks } from '../utils/api';
 import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
+import { useState } from 'react';
 
 export function App() {
+	const [results, setResults] = useState([]);
+	const [hasSearched, setHasSearched] = useState(false);
 	function onSearchSubmit(query) {
 		// Search for the users's query.
-		// TODO: render the results, instead of logging them to the console.
-		// NOTE: `searchArtworks` currently returns local data, so that we
-		// don't make too many requests to the API! Once we've built out
-		// our UI, we need to make real requests!
-		// @see: ./src/uitls/api.js
-		searchArtworks(query).then((json) => {
-			console.log(json);
+		searchArtworks(query).then(({ data }) => {
+			console.log(data);
+			setResults(data);
+			setHasSearched(true);
 		});
 	}
 
@@ -21,6 +21,20 @@ export function App() {
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
+			{results.length ? (
+				<ol>
+					{results.map(({ title, artist_title }) => {
+						return (
+							<li>
+								Title: {title}
+								<br />
+								Artist: {artist_title}
+							</li>
+						);
+					})}
+				</ol>
+			) : null}
+			{hasSearched && results.length === 0 ? <p>No results found</p> : null}
 			<Footer />
 		</div>
 	);
